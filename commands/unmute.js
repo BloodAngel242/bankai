199cfe6442d6a30291ff2945d98d58ba4b5f8b97
BloodAngel242 <angelblood615@gmail.com>
2026-02-19 13:03:46 +0100

    Delete commands directory

--- commands/unmute.js
+++
@@ -1,6 +0,0 @@
-async function unmuteCommand(sock, chatId) {
-    await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: '*ᴄᴇ ɢʀᴏᴜᴘᴇ ᴇsᴛ ᴅᴇ́sᴏʀᴍᴀɪs ᴏᴜᴠᴇʀᴛ́, ᴛᴏᴜs ʟᴇs ᴍᴇᴍʙʀᴇs ᴘᴇᴜᴠᴇɴᴛ ᴇɴᴠᴏʏᴇʀ ᴅᴇs ᴍᴇssᴀɢᴇs 🌹.' });
-}
-
-module.exports = unmuteCommand;
